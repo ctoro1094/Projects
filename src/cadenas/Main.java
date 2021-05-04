6f29a6108d9ad1d6a943e3996e4f7481e3792e7e
ctoro1094 <toro189@hotmail.com>
2021-05-03 21:15:16 -0500

    first

---
+++ src/cadenas/Main.java
@@ -0,0 +1,14 @@
+package cadenas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String cad = "Holaaaaaa";
+		System.out.println("resultado cadena: " + cad);
+		
+
+	}
+
+}
